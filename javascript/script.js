5a3763e148c5949e5e4d5dda44bf0497cc4270c3
adithyan <adithyanpradeep@gmail.com>
2023-11-08 11:20:33 +0530

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+console.log("welcome to javascript")
+console.log(window)
+var i;
+i="hello";
+console.log("i:" ,i)
+var goat ;
+goat="crstiano ronaldo";
+console.log("goat=",goat)
+
+
+var str="this is string";
+console.log("str=",str);
+console.log("type=",typeof str);
+
+
+var num=1000;
+console.log("num=", num);
+console.log("type=", typeof num);
+
+document.write("welcome");

afce177ba2c82dac2c3e1b82271ce929a5d9b5ed
adithyan <adithyanpradeep@gmail.com>
2023-11-09 11:26:51 +0530

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,33 @@
-console.log("welcome to javascript")
-console.log(window)
-var i;
-i="hello";
-console.log("i:" ,i)
-var goat ;
-goat="crstiano ronaldo";
-console.log("goat=",goat)
+// console.log("welcome to javascript")
+// console.log(window)
+// var i;
+// i="hello";
+// console.log("i:" ,i)
+// var goat ;
+// goat="crstiano ronaldo";
+// console.log("goat=",goat)
 
 
-var str="this is string";
-console.log("str=",str);
-console.log("type=",typeof str);
+// var str="this is string";
+// console.log("str=",str);
+// console.log("type=",typeof str);
 
 
-var num=1000;
-console.log("num=", num);
-console.log("type=", typeof num);
+// var num=1000;
+// console.log("num=", num);
+// console.log("type=", typeof num);
 
-document.write("welcome");
+// document.write("welcome");
+
+var str=" hello , world ";
+console.log("uppercase:",str.toUpperCase());
+console.log("length:",str.length)
+console.log("lowercase:",str.toLocaleLowerCase());
+console.log("lenght : ",str.trim().length);
+console.log("lenth :", str.trimEnd().length);
+console.log("length :", str.trimStart().length);
+console.log("substring :" , str.substring(0,5));
+console.log( "replace :",str.replace("hello","haii"));
+console.log("start with:", str.startsWith(" hello"));
+console.log("split :" ,str.split(','));
+console.log("join :",str.split(',').join(','));

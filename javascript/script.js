7bc1367e7f3b9f238ca95b89c270d9a6faa5d4fc
adithyan <adithyanpradeep@gmail.com>
2023-11-15 11:29:03 +0530

    js

--- javascript/script.js
+++ javascript/script.js
@@ -19,15 +19,42 @@
 
 // document.write("welcome");
 
-var str=" hello , world ";
-console.log("uppercase:",str.toUpperCase());
-console.log("length:",str.length)
-console.log("lowercase:",str.toLocaleLowerCase());
-console.log("lenght : ",str.trim().length);
-console.log("lenth :", str.trimEnd().length);
-console.log("length :", str.trimStart().length);
-console.log("substring :" , str.substring(0,5));
-console.log( "replace :",str.replace("hello","haii"));
-console.log("start with:", str.startsWith(" hello"));
-console.log("split :" ,str.split(','));
-console.log("join :",str.split(',').join(','));
+// var str=" hello , world ";
+// console.log("uppercase:",str.toUpperCase());
+// console.log("length:",str.length)
+// console.log("lowercase:",str.toLocaleLowerCase());
+// console.log("lenght : ",str.trim().length);
+// console.log("lenth :", str.trimEnd().length);
+// console.log("length :", str.trimStart().length);
+// console.log("substring :" , str.substring(0,5));
+// console.log( "replace :",str.replace("hello","haii"));
+// console.log("start with:", str.startsWith(" hello"));
+// console.log("split :" ,str.split(','));
+// console.log("join :",str.split(',').join(','));
+
+// {
+//     let b;
+//     b = 15;
+//     const c = 20;
+//     console.log("b : ", b);
+//     console.log("c : ", c);
+// }
+// var obj1 = {
+//     name : "jane",
+//     age : 21,
+//     indian : false,
+// };
+// var json_str = JSON.stringify(obj1);
+// console.log (`Json string : `)
+
+// let num1 = 100;
+// let num2 = 10; 
+
+// if (num1 > num2) {
+//     console,log(`num1 : ${num1} is greater` );
+// } else if (num1 == num2) {
+//     console.log("both are equal");
+
+// }else {
+//     console.log(`num2 : ${num2} is greater`);
+// }

6d6c9fcc6ff423bf2b51aa75a3bcdfcbac1324ec
adithyan <adithyanpradeep36@gmail.com>
2023-12-13 11:28:16 +0530

    js

--- javascript (copy)/12-12/script.js
+++ javascript (copy)/12-12/script.js
@@ -8,11 +8,11 @@
 //event loop
 //call stack
 
-console.log("first line..");
-console.log("second line");
-setTimeout(()=>{
-    console.log("callback function");
-},5000);
+// console.log("first line..");
+// console.log("second line");
+// setTimeout(()=>{
+//     console.log("callback function");
+// },5000);
 
 // let datas = fetch('https://jsonplaceholder.typicode.com/users');
 // console.log(datas);
@@ -20,20 +20,43 @@ setTimeout(()=>{
 console.log("\n\n\nAjax..")
 
 let xhr = new XMLHttpRequest();
-xhr.open("get",'https://jsonplaceholder.typicode.com/users');
+xhr.open("get", 'https://jsonplaceholder.typicode.com/users');
 xhr.send();
 
 console.log("xhr response :",xhr);
 
-xhr.onreadystagechange = function(){
-    console.log("readystate :",xhr.readystate);
-    if(xhr.readyState ==4){
-    console.log("response :",xhr.response);
-    console.log("type of response :", typeof(xhr.response));
+xhr.onreadystatechange = function () {
+    console.log("readystate :", xhr.readyState);
 
-    let parsed_response = JSON.parse(xhr.response);
-    console.log("parsed_response :",parsed_response);
-    console.log("type of parsed_response :",typeof(parsed_response));
+    let content = document.getElementById('content');
+    console.log("content :", content);
+    let datas = '';
+
+
+    if (xhr.readyState == 4) {
+
+        if (xhr.status == 200) {
+            console.log("response :", xhr.response);
+            console.log("type of response :", typeof(xhr.response));
+
+            let parsed_response = JSON.parse(xhr.response);
+            console.log("parsed_response :", parsed_response);
+            console.log("type of parsed_response :", typeof (parsed_response));
+
+            for (let i = 0; i < parsed_response.length; i++) {
+                datas = datas + `<tr>
+    <td>${parsed_response[i].id}</td>
+    <td>${parsed_response[i].name}</td>
+    <td>${parsed_response[i].username}</td>
+    <td>${parsed_response[i].website}</td>
+    </tr>`;
+            }
+            content.innerHTML = datas;
+        } else {
+            console.log("request failed");
+        }
+    } else {
+        console.log("state not completed");
+    }
 }
 
-}
